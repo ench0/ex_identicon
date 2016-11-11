1cb8e34af630cddcc2be5b2aa61d07611c305b57
ICCI Admin <admin@ISLAM.LAN>
2016-11-11 17:07:22 +0000

    upd

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,21 +4,43 @@ defmodule Identicon do
         input
         |> hash_input
         |> pick_color
+        |> build_grid
     end
 
-    def pick_color(image) do
+    def build_grid(%Identicon.Image{hex: hex} = image) do
+        hex
+        |> Enum.chunk(3)
+        # |> mirror_row
+    end
+
+    def mirror_row(row) do
+        
+    end
+
+    def pick_color0(image) do
         %Identicon.Image{hex: hex_list} = image
         [r, g, b | _tail] = hex_list # | _tail = rest of the list
-        [r, g, b]
+        # [r, g, b]
+        # image
+        %Identicon.Image{image | color: {r, g, b}}
+    end
+
+    def pick_color1(image) do
+        %Identicon.Image{hex: [r, g, b | _tail]} = image
+        %Identicon.Image{image | color: {r, g, b}}
+    end
+
+    def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+        %Identicon.Image{image | color: {r, g, b}}
     end
 
     @doc """
-    Creating hash of the string.
+    Creating hash of the string and passing it to Identicon.Image struct.
 
     ## Examples
 
-        iex> Identicon.main("banana")
-        [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+        iex> Identicon.hash_input("banana")
+        %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
 
     """
     def hash_input(input) do
